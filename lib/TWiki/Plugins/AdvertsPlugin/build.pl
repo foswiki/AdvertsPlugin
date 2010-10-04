037b72bbfa158e431c416a3a95bed293b6830c44
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-10-04 15:45:29 +0000

    Item9774: update build.pl so that build will use the extension module for RELEASE/VERSION strings
    git-svn-id: http://svn.foswiki.org/trunk/AdvertsPlugin@9499 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/AdvertsPlugin/build.pl
+++ lib/TWiki/Plugins/AdvertsPlugin/build.pl
@@ -33,7 +33,7 @@ use TWiki::Contrib::Build;
 
   sub new {
     my $class = shift;
-    return bless( $class->SUPER::new( "AdvertsPlugin", "AdvertsPlugin" ), $class );
+    return bless( $class->SUPER::new( "AdvertsPlugin" ), $class );
   }
 
   # Example: Override the build target
